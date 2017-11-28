050a1460be1d39a6684cc70c67514775e3b86d86
SaschaRobel <Sascha.Robel@mailbox.tu-dresden.de>
2017-11-28 22:57:28 +0100

    updated config and launch files

--- conf-data-master/launch.sh
+++ conf-data-master/launch.sh
@@ -1,3 +1,25 @@
+
+#!/bin/sh
+
+# Terminate already running bar instances
+#killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#DISPLAY1=$(xrandr -q | grep 'LVDS1\|VGA-1' | cut -d ' ' -f1)
+#[ ! -z $DISPLAY1 ] & MONITOR=$DISPLAY1 polybar top1 & 
+#echo "top1 " $DISPLAY1 &
+#DISPLAY2=$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)
+#[ ! -z $DISPLAY2 ] & MONITOR=$DISPLAY2 polybar top2 &
+#echo "top2 " $DISPLAY2 & 
+#echo "Bars launched..."
+
+
+
+# this is the default launch script
+
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
@@ -6,8 +28,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+#DISPLAY= "$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
+#[bar/main] && MONITOR= $DISPLAY  &
+#echo "display " $DISPLAY & 
+
 # Launch bar1 and bar2
-polybar example &
-
-
+polybar main & 
+polybar secondary
 echo "Bars launched..."
